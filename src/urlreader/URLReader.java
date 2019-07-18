b6e785701b9bcb90da4ada4138271b0c4e6ba2a0
Jason Fujii <Jasonfujii@yahoo.com>
2019-07-18 13:18:24 -0700

    added lines of code to eextract all of the useful data (i.e. stock info)

--- src/urlreader/URLReader.java
+++ src/urlreader/URLReader.java
@@ -22,25 +22,38 @@ public class URLReader {
         
         //creating a String Builder to catch what is in the website
         StringBuilder text = new StringBuilder();
+        //a string Builder for the good stuff (stock data or anything else)
+        StringBuilder data = new StringBuilder();
         //a String is added to take in the website one line at a time and add it to stringbuilder one at a time
         String line = new String();
         //create the url object
-        URL test = new URL("https://www.hackerrank.com/dashboard");
+        URL test = new URL("https://money.cnn.com/data/markets/");
         //open the connection 
         URLConnection testConnection = test.openConnection();
-        //InputStreamReader
+        //InputStreamReader takes chars and turns them into strings (i think). Here, we wrap the reader into the bufferedreader
+        //for efficiency
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(testConnection.getInputStream()));
         //while there is still something to be read, line equals that next line
         while((line = bufferedReader.readLine()) != null)
         {
             //add the line to the stringbuilder
             text.append(line + "\n");
+            if(line.contains("stock-name"))
+            {
+                data.append(line.substring(line.indexOf("stock-name")) + " - ");
+            }
+            else if(line.contains("stock-price"))
+            {
+                data.append(line.substring(line.indexOf("stock-price")) + "\n");
+            }
         }
         //close the buffered reader now
         bufferedReader.close();
         
         //print out the line
         System.out.println(text);
+        System.out.println("\n \n \n \n");
+        System.out.println(data);
         
         // TODO code application logic here
         
