2593f504d6c7f8370c0a12fc6fd838894100ef90
Jason Fujii <Jasonfujii@yahoo.com>
2019-08-08 06:34:15 -0700

    repository name change

--- src/urlreader/URLReader.java
+++ src/urlreader/URLReader.java
@@ -3,6 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*NEXT TODO:
+    add a writefile method and upload all old information into the file. 
+    then a readfile method to compare the new info to the old info
+    then a compare method which returns if the change was pos or neg and by how much
+*/
+
+
+//I hope this works!!
 package urlreader;
 
 /**
@@ -43,21 +52,17 @@ public class URLReader {
             //add the line to the stringbuilder
             
             text.append(line + "\n");
+            //if the line contains the stock name variable, take it's value and append it to data
             if(line.contains("stock-name"))
             {
                 int num1, num2;
                 num1 = line.indexOf("stock-name") + 12;
-                
-                
-                
                 one = line.substring(num1);
                 num2 = one.indexOf("<");
                 two = one.substring(0,num2);
-                //+ 11, line.indexOf("<") - 1);
-                //two = line.substring(line.indexOf("<") - 1);
-                //data.append(line.substring(line.indexOf("stock-name")) + " - ");
                 data.append(two + " - ");
             }
+            //if the line contains the stock price variable, take its value and append it to data
             else if(line.contains("stock-price"))
             {
                 int num3, num4;
@@ -72,24 +77,7 @@ public class URLReader {
         }
         //close the buffered reader now
         bufferedReader.close();
-        
-        //print out the line
-        //System.out.println(text);
-        //System.out.println("\n \n \n \n");
         System.out.println(data);
-        
-        // TODO code application logic here
-        
-        /*URL oracle = new URL("http://www.oracle.com/");
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
-        
-        System.out.println("this works");
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
-        System.out.println("this works 2");*/
     }
     
 }
